9eb011af5c61d3631fdad397b1fffa86555a0357
Harascho1 <luka.old1@gmail.com>
2025-12-24 22:46:12 +0100

    add spinning cup and menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Activity, useEffect, useRef, useState } from "react";
 import LoadingSpinner from "./components/LoadingSpiner";
+import Spin90 from "./components/SpinCup";
 import "./App.css";
 import "./components/Navbar.css";
 
@@ -10,9 +11,12 @@ import Cola1 from "./assets/cocacola1.jpg";
 import Cola2 from "./assets/cocacola2.jpg";
 import Beer1 from "./assets/beer1.jpg";
 import Home from "./assets/home.jpg";
+import Cup from "./assets/cup.png";
+import Insta from "./assets/insta.svg";
 
 function App() {
   const homeRef = useRef(null);
+  const menuRef = useRef(null);
   const galleryRef = useRef(null);
   const aboutUsRef = useRef(null);
 
@@ -55,6 +59,9 @@ function App() {
           <ul>
             <li onClick={() => scrollToSection(homeRef)}>Home</li>
             <li onClick={() => scrollToSection(galleryRef)}>Galerija</li>
+            <li onClick={() => scrollToSection(menuRef)}>
+              <a href="https://app.emeni.rs/1435">Cenovnik</a>
+            </li>
             <li onClick={() => scrollToSection(aboutUsRef)}>O nama</li>
           </ul>
         </nav>
@@ -66,7 +73,10 @@ function App() {
             className="home-section"
           >
             <img src={Home} className="home-photo" />
-            <h1 className="brand-name">Bar Barezi</h1>
+            <div className="brand-name">
+              <h1>Bar Barezi</h1>
+              <Spin90 img={Cup} />
+            </div>
           </section>
 
           <section
@@ -94,16 +104,21 @@ function App() {
           >
             <h2>O nama</h2>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              <span className="text">
+                Smešten u Kovinu, kafić Barezi predstavlja savršen spoj urbanog
+                dizajna i tople, domaće atmosfere. Naša misija je jednostavna:
+                pružiti svakom gostu trenutak predaha uz najkvalitetniju ponudu
+                pića i vrhunsku uslugu. Od pažljivo biranih zrna kafe do bogate
+                karte pića, svaki detalj u Bareziju osmišljen je da zadovolji
+                čula. Naš prostor je idealan za sve prilike – od mirnih jutara
+                uz novine do energičnih večernjih druženja.
+              </span>
+            </p>
+            <p>Addresa: Cara Lazara 84, Kovin</p>
+            <p>
+              <a href="https://www.instagram.com/dzb.caffe_lounge_bar_barezi/?hl=en">
+                <img src={Insta} width="50px" />
+              </a>
             </p>
           </section>
         </main>
---
+++ src/components/SpinCup.jsx
@@ -0,0 +1,11 @@
+import "./SpinCup.css";
+
+const Spin90 = ({ img }) => {
+  return (
+    <div className="spin-90-container">
+      <img src={img} className="spin-90" />
+    </div>
+  );
+};
+
+export default Spin90;
